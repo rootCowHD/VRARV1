0ad5e4da971a782c56a4d48557a43fe80f0eb1f4
rootCowHD <stefanwerntges@gmail.com>
2017-11-13 22:22:04 +0100

    Initial commit

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-vuforia.VuforiaPlugin",
+    "file": "plugins/cordova-plugin-vuforia/www/VuforiaPlugin.js",
+    "pluginId": "cordova-plugin-vuforia",
+    "clobbers": [
+      "navigator.VuforiaPlugin"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-vuforia-sdk": "6.0.117",
+  "cordova-plugin-vuforia": "3.2.1"
+};
+// BOTTOM OF METADATA
+});
